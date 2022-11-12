f6c07fcce4f8846bbe13f1e5fa0330612bb95733
aaron <alkrauss48@gmail.com>
2022-11-12 14:00:27 -0600

    Add border-t-white to safelist since it's generated dynamically

--- themes/thecodeboss/tailwind.config.js
+++ themes/thecodeboss/tailwind.config.js
@@ -2,6 +2,9 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: ["content/**/*.md", "layouts/**/*.html"],
+  safelist: [
+    'border-t-white',
+  ],
   theme: {
     extend: {
       borderWidth: {
