a7ef58b4207b99703e65dbc0fe7fc92acacd1107
aaron <alkrauss48@gmail.com>
2022-11-19 22:03:55 -0600

    Install three.js and create home script

---
+++ themes/thecodeboss/assets/js/home.ts
@@ -0,0 +1,3 @@
+import { Scene } from 'three';
+
+const scene = new Scene();
--- themes/thecodeboss/rollup.config.js
+++ themes/thecodeboss/rollup.config.js
@@ -1,10 +1,26 @@
 import typescript from '@rollup/plugin-typescript';
 
-export default {
-  input: 'assets/js/main.ts',
-  output: {
-    file: 'assets/dist/bundle.js',
-    format: 'iife'
-  },
-  plugins: [typescript()]
+const output = {
+  dir: 'assets/dist',
+  format: 'iife'
 };
+
+const plugins = [typescript()];
+
+export default [{
+  input: 'assets/js/main.ts',
+  output,
+  plugins,
+}, {
+  external: [
+    'three',
+  ],
+  output: {
+    globals: {
+      'three': 'three',
+    },
+  },
+  input: 'assets/js/home.ts',
+  output,
+  plugins,
+}];
