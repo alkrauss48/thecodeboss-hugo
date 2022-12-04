d1e3b42cf7848a9c5eead0562b5cdf6fc8e9ac94
aaron <alkrauss48@gmail.com>
2022-12-03 22:02:51 -0600

    Update tailwind safelist comment paths

--- themes/thecodeboss/tailwind/safelist.js
+++ themes/thecodeboss/tailwind/safelist.js
@@ -1,23 +1,23 @@
 const COLORS = ['teal', 'blue', 'orange'];
 
 module.exports = [
-  // themes/thecodeboss/layouts/partials/arrow-down.html
+  // ../layouts/partials/arrow-down.html
   'border-t-white',
   'border-t-dark',
 
   ...COLORS.map((color) => [
 
-    // themes/thecodeboss/layouts/partials/home/community.html
-    // themes/thecodeboss/layouts/partials/home/portfolio.html
+    // ../layouts/partials/home/community.html
+    // ../layouts/partials/home/portfolio.html
     `bg-${color}`,
 
-    // themes/thecodeboss/layouts/partials/home/community.html
+    // ../layouts/partials/home/community.html
     `bg-${color}-200`,
     `hover:text-${color}`,
     `focus:text-${color}`,
     `border-${color}`,
 
-    // themes/thecodeboss/layouts/partials/home/portfolio.html
+    // ../layouts/partials/home/portfolio.html
     `hover:bg-${color}-500`,
     `focus:bg-${color}-500`,
 
